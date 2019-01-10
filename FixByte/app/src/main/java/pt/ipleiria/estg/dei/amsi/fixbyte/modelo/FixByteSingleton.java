3d081920ac69a63941642d513a3fbd9af023b557
pedromig123 <pedropedro_miguel@hotmail.com>
2019-01-10 16:11:05 +0000

    fix last commit

--- FixByte/app/src/main/java/pt/ipleiria/estg/dei/amsi/fixbyte/modelo/FixByteSingleton.java
+++ FixByte/app/src/main/java/pt/ipleiria/estg/dei/amsi/fixbyte/modelo/FixByteSingleton.java
@@ -9,45 +9,48 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import pt.ipleiria.estg.dei.amsi.fixbyte.listeners.FixByteListener;
+import pt.ipleiria.estg.dei.amsi.fixbyte.listeners.LoginListener;
 import pt.ipleiria.estg.dei.amsi.fixbyte.listeners.UserListener;
 import pt.ipleiria.estg.dei.amsi.fixbyte.utils.FixByteJsonParser;
 
-public class FixByteSingleton implements FixByteListener, UserListener {
+public class FixByteSingleton implements FixByteListener, LoginListener {
     private static FixByteSingleton INSTANCE = null;
 
     private ArrayList<Campanha> campanhas;
     private ArrayList<ProdutoCampanha> produtoscampanha;
-    private ArrayList<User> users;
-    private ArrayList<Userdata> usersdata;
+
     private ArrayList<Categoria> categorias;
+    private User user;
 
 
-
-    private ProdutoCampanhaBDHelper produtocampanhaBDHelper = null;
     private CampanhaBDHelper campanhaBDHelper = null;
     private UserBDHelper userBDHelper = null;
     private UserdataBDHelper userdataBDHelper = null;
     private CategoriaBDHelper categoriaBDHelper = null;
 
 
-    //private String mUrlAPICampanhas = "http://192.168.1.69:8888/v1/campanhas";
-    private String mUrlAPIProdutosCampanhas = "http://192.168.137.1:8888/v1/campanhas/";
-    private String mUrlAPICampanhas = "http://192.168.137.1:8888/v1/campanhas";
+    //private String mUrlAPICampanhas = "http://10.20.140.21:8888/v1/campanhas";
+    private String mUrlAPIProdutosCampanhas = "http://10.20.140.21:8888/v1/campanhas/";
+    private String mUrlAPICampanhas = "http://10.20.140.21:8888/v1/campanhas";
     private String mUrlAPIUser = "http://192.168.137.1:8888/v1/users";
     private String mUrlAPIUserData = "http://192.168.137.1:8888/v1/users";
     private String mUrlAPICategorias = "http://192.168.137.1:8888/v1/categorias";
-
+    private String APILogin = "http://10.20.140.21:8888/v1/users/login";
     private static RequestQueue volleyQueue;
 
     private FixByteListener fixByteListener;
-
+    private LoginListener loginListener;
 
 
     public static synchronized FixByteSingleton getInstance(Context context)
@@ -65,7 +68,6 @@ public class FixByteSingleton implements FixByteListener, UserListener {
         campanhaBDHelper = new CampanhaBDHelper(context);
 
         produtoscampanha = new ArrayList<>();
-        produtocampanhaBDHelper = new ProdutoCampanhaBDHelper(context);
 
         categorias = new ArrayList<>();
         categoriaBDHelper = new CategoriaBDHelper(context);
@@ -106,16 +108,12 @@ public class FixByteSingleton implements FixByteListener, UserListener {
 
         if (!isConnected){
             campanhas = campanhaBDHelper.getAllCampanhasBD();
-            System.out.println("CAMPANHAS1: " + campanhas);
-
             if (!campanhas.isEmpty()){
                 if(fixByteListener != null)
                 {
                     fixByteListener.onRefreshListaCampanhas(campanhas);
                 }
             }
-
-
         }else{
             JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, mUrlAPICampanhas, null, new Response.Listener<JSONArray>() {
 
@@ -128,7 +126,6 @@ public class FixByteSingleton implements FixByteListener, UserListener {
                     {
                         campanhaBDHelper.removeAllCampanhas();
                         adicionarCampanhasBD(campanhas);
-
                         fixByteListener.onRefreshListaCampanhas(campanhas);
                     }
                 }
@@ -136,13 +133,10 @@ public class FixByteSingleton implements FixByteListener, UserListener {
                 @Override
                 public void onErrorResponse(VolleyError error){
                     System.out.println("ERROR: " + error);
-
                 }
-
             });
             volleyQueue.add(req);
         }
-
     }
     public void removerCampanha (long idCampanha)
     {
@@ -210,7 +204,7 @@ public class FixByteSingleton implements FixByteListener, UserListener {
     {
         for (ProdutoCampanha produtocampanha : produtoscampanha)
         {
-            if (produtocampanha.getIdprodutocampanha() == idprodutocampanha)
+            if (produtocampanha.getIdprodutos() == idprodutocampanha)
             {
                 return produtocampanha;
             }
@@ -218,55 +212,24 @@ public class FixByteSingleton implements FixByteListener, UserListener {
         return null;
     }
 
-    public void adicionarProdutoCampanhaBD(ProdutoCampanha produtocampanha)
-    {
-        produtocampanhaBDHelper.adicionarProdutoCampanhaBD(produtocampanha);
-    }
-
-    public void adicionarProdutosCampanhaBD(ArrayList<ProdutoCampanha> listaProdutoscampanha)
-    {
-        produtocampanhaBDHelper.removeAllProdutosCampanha();
-        for (ProdutoCampanha produtocampanha : produtoscampanha)
-        {
-            adicionarProdutoCampanhaBD(produtocampanha);
-        }
-    }
-
-
     public void getAllProdutoCampanhaAPI (final Context context, boolean isConnected, Long idCampanha)
     {
         Toast.makeText(context, "isConnected: " + isConnected, Toast.LENGTH_SHORT).show();
 
-        if(!isConnected)
+        String mUrlAPIProdutosCampanhas1;
+        mUrlAPIProdutosCampanhas1 = null;
+        mUrlAPIProdutosCampanhas1 = mUrlAPIProdutosCampanhas;
+        mUrlAPIProdutosCampanhas1 += idCampanha;
+        mUrlAPIProdutosCampanhas1 += "/produtos";
+
+        if(isConnected)
         {
-            produtoscampanha = produtocampanhaBDHelper.getAllProdutoCampanhasBD(idCampanha);
-
-            if (!produtoscampanha.isEmpty()){
-                if(fixByteListener != null)
-                {
-                    fixByteListener.onRefreshListaProdutosCampanha(produtoscampanha);
-                }
-            }
-        }else{
-            mUrlAPIProdutosCampanhas += idCampanha;
-            mUrlAPIProdutosCampanhas += "/produtos";
-            System.out.println("--> RESPOSTA: " + mUrlAPIProdutosCampanhas);
-
-            JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, mUrlAPIProdutosCampanhas, null, new Response.Listener<JSONArray>() {
+            JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, mUrlAPIProdutosCampanhas1, null, new Response.Listener<JSONArray>() {
                 @Override
                 public void onResponse(JSONArray response) {
                     System.out.println("--> RESPOSTA: " + response);
-
                     produtoscampanha = FixByteJsonParser.parserJsonProdutosCampanha(response,context);
-                    adicionarProdutosCampanhaBD(produtoscampanha);
-
-                    if(fixByteListener != null)
-                    {
-                        produtocampanhaBDHelper.removeAllProdutosCampanha();
-                        adicionarProdutosCampanhaBD(produtoscampanha);
-                        fixByteListener.onRefreshListaProdutosCampanha(produtoscampanha);
-                    }
-
+                    fixByteListener.onRefreshListaProdutosCampanha(produtoscampanha);
                 }
             }, new Response.ErrorListener(){
                 @Override
@@ -277,36 +240,7 @@ public class FixByteSingleton implements FixByteListener, UserListener {
             volleyQueue.add(req);
         }
     }
-    public void removerProdutocampanha (long idprodutocampanha)
-    {
-        ProdutoCampanha produticampanhaaux = getProdutoCampanha(idprodutocampanha);
-        if (produticampanhaaux!=null)
-        {
-            if (produtocampanhaBDHelper.removerProdutoCampanhaBD((produticampanhaaux.getIdprodutocampanha())))
-            {
-                produtoscampanha.remove(produticampanhaaux);
-            }
-        }
-    }
 
-    public void editarProdutoCampanha (ProdutoCampanha produtoCampanha)
-    {
-        if (!produtoscampanha.contains(produtoCampanha))
-        {
-            return;
-        }
-
-        ProdutoCampanha produtocampanhaux = getProdutoCampanha(produtoCampanha.getIdprodutocampanha());
-        produtocampanhaux.setIdprodutocampanha(produtoCampanha.getCampanha_idCampanha());
-        produtocampanhaux.setProdutos_idprodutos(produtoCampanha.getProdutos_idprodutos());
-        produtocampanhaux.setCampanha_idCampanha(produtoCampanha.getCampanha_idCampanha());
-
-
-        if (produtocampanhaBDHelper.editarProdutoCampanhaBD(produtoCampanha))
-        {
-            System.out.println("PRODUTOSCAMPANHAS ADICIONADAS");
-        }
-    }
     public void setProdutosCampanhaListener (FixByteListener fixByteListener )
     {
         this.fixByteListener = fixByteListener;
@@ -321,146 +255,53 @@ public class FixByteSingleton implements FixByteListener, UserListener {
     @Override
     public void onUpdateListaProdutosCampanhaBD(ProdutoCampanha produtocampanha, int operacao)
     {
-        switch (operacao){
-            case 1: adicionarProdutoCampanhaBD(produtocampanha);
-                break;
-            case 2: editarProdutoCampanha(produtocampanha);
-                break;
-            case 3: removerCampanha(produtocampanha.getIdprodutocampanha());
-                break;
-        }
+
     }
     //endregion
 
-    //region user
-    public ArrayList<User> getUser()
-    {
-        return new ArrayList<>(users);
-    }
-
-    public User getUser (long id)
-    {
-        for (User user : users)
-        {
-            if (user.getId() == id)
-            {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    public void adicionarUserBD(User user)
-    {
-        userBDHelper.adicionarUserBD(user);
-    }
-
-    public void adicionarUserBD(ArrayList<User> listaUser)
-    {
-        userBDHelper.removeAllUsers();
-        for (User user : users)
-        {
-            adicionarUserBD(user);
-        }
-    }
+    public void APILogin(final Context context, final String username, final String password){
 
 
-    public void getAllUserAPI (final Context context, boolean isConnected)
-    {
-        Toast.makeText(context, "isConnected: " + isConnected, Toast.LENGTH_SHORT).show();
+        StringRequest req = new StringRequest
+                (Request.Method.POST, APILogin, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println("--> RESPOSTA ADD POST : "+response);
 
-        if(!isConnected)
-        {
-            users = userBDHelper.getAllUsersBD();
 
-            if (!users.isEmpty()){
-                if(fixByteListener != null)
-                {
-                    //fixByteListener.onRefreshListaUsers(users);
-                }
-            }
-        }else{
-            mUrlAPIUser += 1;
-            mUrlAPIUser += "/produtos";
-            System.out.println("--> RESPOSTA: " + mUrlAPIUser);
+                        if (response.equals("false")){
+                            user = null;
+                        }else{
+                            user = FixByteJsonParser.parserJsonLogin(response,context);
 
-            JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, mUrlAPIUser, null, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    System.out.println("--> RESPOSTA: " + response);
-
-                    users = FixByteJsonParser.parserJsonUser(response,context);
-                    adicionarUserBD(users);
-
-                    if(fixByteListener != null)
-                    {
-                        produtocampanhaBDHelper.removeAllProdutosCampanha();
-                        adicionarProdutosCampanhaBD(produtoscampanha);
-                        fixByteListener.onRefreshListaProdutosCampanha(produtoscampanha);
+                            if (user != null){
+                                if(loginListener != null)
+                                {
+                                    loginListener.onUpdateLogin(true, user.getToken());
+                                }
+                            }else{
+                                if(loginListener != null) {
+                                    loginListener.onUpdateLogin(false, null);
+                                }
+                            }
+                        }
                     }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d( "Errorr ADD: " + error.getMessage());
+                    }
+                }) {
 
-                }
-            }, new Response.ErrorListener(){
-                @Override
-                public void onErrorResponse(VolleyError error){
-                    System.out.println("ERROR: " + error);
-                }
-            });
-            volleyQueue.add(req);
-        }
-    }
-    public void removerUser (long id)
-    {
-        User useraux = getUser(id);
-        if (useraux!=null)
-        {
-            if (userBDHelper.removerUserBD((useraux.getId())))
-            {
-                users.remove(useraux);
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
             }
-        }
-    }
-
-    public void editarUser (User user)
-    {
-        if (!users.contains(user))
-        {
-            return;
-        }
-
-        User useraux = getUser(user.getId());
-        useraux.setUsername(user.getUsername());
-        useraux.setPassword_hash(user.getPassword_hash());
-        useraux.setEmail(user.getEmail());
-
-
-        if (userBDHelper.editarUserBD(useraux))
-        {
-            System.out.println("USER ADICIONADO");
-        }
-    }
-    public void setUserListener (FixByteListener fixByteListener )
-    {
-        this.fixByteListener = fixByteListener;
-    }
-
-    @Override
-    public void onRefreshListaUsers(ArrayList<User> listausers)
-    {
-
-    }
-
-    @Override
-    public void onUpdateListaUsersBD(User user, int operacao)
-    {
-        switch (operacao){
-            case 1: adicionarUserBD(user);
-                break;
-            case 2: editarUser(user);
-                break;
-            case 3: removerUser(user.getId());
-                break;
-        }
+        };
+        volleyQueue.add(req);
     }
     //endregion
 
@@ -548,6 +389,14 @@ public class FixByteSingleton implements FixByteListener, UserListener {
     public void onUpdateListaCategoriasBD(Categoria categoria, int operacao)
     {
 
+    }
+
+    public void setUpdateLogin(LoginListener loginListener) {
+        this.loginListener = loginListener;
+    }
+
+    @Override
+    public void onUpdateLogin(boolean key, String token) {
     }
     //endregion
 }
